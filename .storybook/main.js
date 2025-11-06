b14d5112cf259b30c5b3de4582ed2c5b4fedaaa0
Andrew Borovin <andrew.borovin@reltio.com>
2025-11-06 13:50:47 +0000

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
