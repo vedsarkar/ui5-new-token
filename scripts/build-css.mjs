28a455e90f8ab90cc08ce171ff7dbb494cdc9f15
Andrew Borovin <andrew.borovin@reltio.com>
2026-02-17 09:50:33 +0000

    update css build

--- scripts/build-css.mjs
+++ scripts/build-css.mjs
@@ -2,6 +2,12 @@ import fs from "node:fs";
 import postcss from "postcss";
 import postcssModules from "postcss-modules";
 
+const generateScopedName = (name, filename) => {
+	const componentName = filename.split("/").pop()?.replace(/\.module\.css$/, "");
+	const hash = Buffer.from(`${filename}${name}`).toString("base64").slice(0, 5);
+	return `${componentName}_${name}__${hash}`;
+};
+
 const template = (result) => `import styleInject from 'style-inject';
 import json from './${result.opts.from.split("/").pop()}.json';
 styleInject(\`\n${result.css}\`);
@@ -9,12 +15,14 @@ export default json;
 `;
 
 fs.glob(
-	["../../components/**/*.module.css", "../../icons/**/*.module.css"],
+	["../components/**/*.module.css", "../icons/**/*.module.css"],
 	(err, matches) => {
 		if (err) throw err;
 		matches.forEach(async (path) => {
 			const css = fs.readFileSync(path);
-			const result = await postcss([postcssModules]).process(css, {
+			const result = await postcss([
+				postcssModules({ generateScopedName }),
+			]).process(css, {
 				from: path,
 			});
 			fs.writeFileSync(`${path}.ts`, template(result));
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	css: {
+		modules: {
+			generateScopedName: (name, filename) => {
+				const componentName = filename
+					.split("/")
+					.pop()
+					?.replace(/\.module\.css$/, "");
+				const hash = Buffer.from(`${filename}${name}`).toString("base64").slice(0, 5);
+				return `${componentName}_${name}__${hash}`;
+			},
+		},
+	},
+});
