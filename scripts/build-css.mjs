686ac86dda7f029b892b1c694953633f39454367
Andrew Borovin <andrew.borovin@reltio.com>
2026-02-17 14:31:44 +0000

    update build script

--- scripts/build-css.mjs
+++ scripts/build-css.mjs
@@ -3,7 +3,10 @@ import postcss from "postcss";
 import postcssModules from "postcss-modules";
 
 const generateScopedName = (name, filename) => {
-	const componentName = filename.split("/").pop()?.replace(/\.module\.css$/, "");
+	const componentName = filename
+		.split("/")
+		.pop()
+		?.replace(/\.module\.css$/, "");
 	const hash = Buffer.from(`${filename}${name}`).toString("base64").slice(0, 5);
 	return `${componentName}_${name}__${hash}`;
 };
@@ -14,18 +17,15 @@ styleInject(\`\n${result.css}\`);
 export default json;
 `;
 
-fs.glob(
-	["../components/**/*.module.css", "../icons/**/*.module.css"],
-	(err, matches) => {
-		if (err) throw err;
-		matches.forEach(async (path) => {
-			const css = fs.readFileSync(path);
-			const result = await postcss([
-				postcssModules({ generateScopedName }),
-			]).process(css, {
-				from: path,
-			});
-			fs.writeFileSync(`${path}.ts`, template(result));
+fs.glob("**/*.module.css", (err, matches) => {
+	if (err) throw err;
+	matches.forEach(async (path) => {
+		const css = fs.readFileSync(path);
+		const result = await postcss([
+			postcssModules({ generateScopedName }),
+		]).process(css, {
+			from: path,
 		});
-	},
-);
+		fs.writeFileSync(`${path}.ts`, template(result));
+	});
+});
